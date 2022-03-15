95820df3c2b7f026c680e4a09b550aa934914ad6
Dmitriy <idimka2210@gmail.com>
2022-03-15 20:31:14 +0300

    add spring cache

--- src/main/java/com/example/demo/RegionListController.java
+++ src/main/java/com/example/demo/RegionListController.java
@@ -5,39 +5,25 @@ import com.example.demo.model.Region;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
-
 @RestController
 public class RegionListController {
 
     @Autowired
     RegionMapper regionMapper;
 
-    Map<String, Function<String, Region>> regionFieldToGetMap;
-
-    public RegionListController() {
-        regionFieldToGetMap = new HashMap<>();
-
-        regionFieldToGetMap.put("id", (id) -> regionMapper.getById(id));
-        regionFieldToGetMap.put("name", (name) -> regionMapper.getByName(name));
-        regionFieldToGetMap.put("shortname", (shortname) -> regionMapper.getByShortname(shortname));
-    }
-
-    /*Добавление региона в бд*/
+    /*Добавление региона в бд. Принимает json добавляемого объекта*/
     @PostMapping
     public Integer addRegion(@RequestBody Region region) {
         return regionMapper.add(region);
     }
 
-    /*Получение региона по полю (id, name, shortname)*/
-    @GetMapping("/{field}")
-    public Region getById(@PathVariable("field") String field, @RequestParam String value) {
-        return regionFieldToGetMap.get(field).apply(value);
+    /*Получение региона по id*/
+    @GetMapping
+    public Region getById(@RequestParam String id) {
+        return regionMapper.getById(id);
     }
 
-    /*Изменение региона по его id. Берём id из тела запроса и обновляем сущность на новые значения*/
+    /*Изменение региона по его id*/
     @PatchMapping
     public void updateById(@RequestBody Region region) {
         regionMapper.update(region);
--- src/main/java/com/example/demo/mapper/RegionMapper.java
+++ src/main/java/com/example/demo/mapper/RegionMapper.java
@@ -5,26 +5,28 @@ import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@CacheConfig(cacheNames={"regions"})
 public interface RegionMapper {
 
+    @Cacheable(key = "#id")
     @Select("SELECT * FROM region WHERE id = #{id}")
     Region getById(String id);
 
-    @Select("SELECT * FROM region WHERE name = #{name}")
-    Region getByName(String name);
-
-    @Select("SELECT * FROM region WHERE shortname = #{shortname}")
-    Region getByShortname(String shortname);
-
+    @Cacheable(key = "#region.id")
     @Insert("INSERT INTO region(id, name, shortname) VALUES (#{id}, #{name}, #{shortName})")
     Integer add(Region region);
 
+    @CacheEvict(key = "#region.id")
     @Update("UPDATE region SET name = #{name}, shortname = #{shortName} WHERE id = #{id}")
     void update(Region region);
 
+    @CacheEvict(key = "#id")
     @Delete("DELETE FROM region WHERE id = #{id}")
     void deleteById(String id);
 }
